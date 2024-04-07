ea279051ee32448130e55f551289aef10b5a7732
masatma1 <masatma1@fel.cvut.cz>
2024-04-07 22:41:55 +0200

    LinkedList: Add InsertAfter function.

--- utils/linked_list.go
+++ utils/linked_list.go
@@ -1,27 +1,36 @@
 package utils
 
-type Node[T any] struct {
+type Node[T string | int] struct {
 	value T
 	next  *Node[T]
 }
 
-type LinkedList[T any] struct {
+type LinkedList[T string | int] struct {
 	head *Node[T]
 	tail *Node[T]
 	Size int `default:"0"`
 }
 
-func findNewTail[T any](list *LinkedList[T]) *Node[T] {
+func findFirstNodeByValue[T string | int](list *LinkedList[T], value T) *Node[T] {
+	var current = list.head
+	for current != nil && current.value != value {
+		current = current.next
+	}
+
+	return current
+}
+
+func findNewTail[T string | int](list *LinkedList[T]) *Node[T] {
 	var newTail = list.head
 
-	for newTail.next != nil {
+	for newTail.next != list.tail {
 		newTail = newTail.next
 	}
 
 	return newTail
 }
 
-func Push[T any](list *LinkedList[T], value T) {
+func Push[T string | int](list *LinkedList[T], value T) {
 	var newNode = &Node[T]{
 		value: value,
 		next:  nil,
@@ -39,7 +48,26 @@ func Push[T any](list *LinkedList[T], value T) {
 
 }
 
-func Pop[T any](list *LinkedList[T]) T {
+func InsertAfter[T string | int](list *LinkedList[T], o T, after T) {
+	var newNode = &Node[T]{
+		value: o,
+		next:  nil,
+	}
+
+	if list.tail.value == after {
+		list.tail.next = newNode
+		list.tail = newNode
+		list.Size++
+	} else {
+		var current = findFirstNodeByValue(list, after)
+		var oldNext = current.next
+		current.next = newNode
+		newNode.next = oldNext
+		list.Size++
+	}
+}
+
+func Pop[T string | int](list *LinkedList[T]) T {
 	var toReturn T
 	switch list.Size {
 	case 0:
@@ -59,3 +87,22 @@ func Pop[T any](list *LinkedList[T]) T {
 
 	return toReturn
 }
+
+/*
+func PrintList[T string | int](list *LinkedList[T]) {
+	var b bytes.Buffer
+	b.WriteString("[")
+
+	var current = list.head
+	for current != nil {
+		if current != list.head {
+			b.WriteString(", ")
+		}
+
+		b.WriteString(current.value) //TOOD problem with generic
+		current = current.next
+	}
+
+	b.WriteString("]")
+	fmt.Println(b.String())
+} */
--- utils/linked_list_test.go
+++ utils/linked_list_test.go
@@ -7,20 +7,47 @@ func TestPush(t *testing.T) {
 	Push(&list, "hello")
 	Push(&list, "world")
 
-	if list.Size != 2 {
-		t.Errorf("got %q, wanted %q", list.Size, 2)
-	}
+	checkEquals(list.Size, 2, t)
 }
 
 func TestPop(t *testing.T) {
-	var expected = "test"
 	list := LinkedList[string]{}
 
 	Push(&list, "hello")
 	Push(&list, "world")
-	Push(&list, expected)
+	Push(&list, "test")
 
-	var got = Pop(&list)
+	checkEquals(Pop(&list), "test", t)
+	checkEquals(Pop(&list), "world", t)
+	checkEquals(Pop(&list), "hello", t)
+}
+
+func TestSize(t *testing.T) {
+	list := LinkedList[string]{}
+	checkEquals(list.Size, 0, t)
+
+	Push(&list, "hello")
+	Push(&list, "world")
+	Push(&list, "test")
+	checkEquals(list.Size, 3, t)
+
+	Pop(&list)
+	Pop(&list)
+	checkEquals(list.Size, 1, t)
+}
+
+func TestInsertAfter(t *testing.T) {
+	list := LinkedList[string]{}
+	Push(&list, "hello")
+	InsertAfter(&list, "world", "hello")
+	InsertAfter(&list, "test", "world")
+
+	checkEquals(Pop(&list), "test", t)
+	checkEquals(Pop(&list), "world", t)
+	checkEquals(Pop(&list), "hello", t)
+}
+
+func checkEquals[T string | int](got T, expected T, t *testing.T) {
 	if got != expected {
 		t.Errorf("got %q, wanted %q", got, expected)
 	}
